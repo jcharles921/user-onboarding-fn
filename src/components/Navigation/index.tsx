2904049c3f62f82fc46f3fa3e9f9af8981c6f2eb
jcharles921 <babouwitonze921@gmail.com>
2024-10-18 22:56:36 +0200

    Fixing apollo client hydration

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import style from "@/styles/navigation.module.css";
+const Navigation = () => {
+  return <div className={style.navigationBox}>index</div>;
+};
+
+export default Navigation;
